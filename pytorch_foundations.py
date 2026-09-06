7691a5b9b0dff1516b3b4e82085201363c0b6987
Tetyana Hvozda <tetyanahvozda@Tetyanas-MBP.attlocal.net>
2026-09-06 15:31:18 -0400

    added matrix  multiplication

--- pytorch_foundations.py
+++ pytorch_foundations.py
@@ -24,3 +24,34 @@ print(float_32_tensor.dtype)
 
 float_16_tensor = float_32_tensor.type(torch.float16)
 print(float_16_tensor.dtype)
+
+# Getting information from a tensor
+tensor = torch.rand(3, 4)
+print(f"Data type of tensor: {tensor.dtype}")
+print(f"hape of a tensor: {tensor.shape}")
+print(f"Device of a tensor: {tensor.device}")
+
+# Addition
+tensor = torch.tensor([1, 2, 3])
+tensor = tensor + 100
+print(tensor)
+
+# Multiply
+tensor = tensor * 10
+print(tensor)
+
+# Subtract
+tensor = tensor - 10
+print(tensor)
+
+# PyTorch in-built functions
+tensor = torch.mul(tensor, 10)
+print(tensor)
+
+tensor = torch.add(tensor, 10)
+print(tensor)
+
+# Matrix multiplication
+tensor = torch.tensor([1, 2, 3])
+print(tensor * tensor)
+print(torch.matmul(tensor, tensor))

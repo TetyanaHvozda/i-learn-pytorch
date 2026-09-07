42ff3ee1a6af6bde18ef35f68e8ccf7d59605f59
Tetyana Hvozda <tetyanahvozda@Tetyanas-MBP.attlocal.net>
2026-09-07 14:18:20 -0400

    aded reproducability

--- pytorch_foundations.py
+++ pytorch_foundations.py
@@ -187,3 +187,24 @@ numpy_tensor = tensor.numpy()
 print(tensor.dtype)
 print(numpy_tensor.dtype)
 
+# change the tensor, what happens to numpy
+tensor = tensor + 1
+print(tensor)
+print(numpy_tensor)
+
+# reproducability (take random out of random)
+random_tensor_A = torch.rand(3, 4)
+random_tensor_B = torch.rand(3, 4)
+
+print(random_tensor_A)
+print(random_tensor_B)
+print(random_tensor_A == random_tensor_B)
+
+RANDOM_SEED = 42
+torch.manual_seed(RANDOM_SEED) # works only for one block of code
+random_tensor_C = torch.rand(3, 4)
+torch.manual_seed(RANDOM_SEED)
+random_tensor_D = torch.rand(3, 4)
+print(random_tensor_C)
+print(random_tensor_D)
+print(random_tensor_C == random_tensor_D)

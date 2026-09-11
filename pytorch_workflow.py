2850fb410861c0eba43b98062568b79777a37d59
Tetyana Hvozda <tetyanahvozda@gmail.com>
2026-09-11 09:17:03 -0400

    added linear regression model

--- pytorch_workflow.py
+++ pytorch_workflow.py
@@ -54,3 +54,16 @@ def plot_predictions(train_data=X_train,
 
 plot_predictions()
 plt.show()
+
+# Build a PyTorch model
+class LinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.weights = nn.Parameter(torch.randn(1,
+                                                requires_grad=True,
+                                                dtype=torch.float))
+        self.bias = nn.Parameter(torch.randn(1,
+                                             requires_grad=True,
+                                             dtype=torch.float))
+        def forward(self, x: torch.Tensor) -> torch.Tensor:
+            return self.weights * x + self.bias
